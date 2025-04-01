40a2ec9697a7d389b47f5b4600183808d2bcfb69
andkcode <akiricuk37@gmail.com>
2025-04-01 19:13:35 +0200

    installing dependencies

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,7 @@
 <template>
-    <div class="container flex min-h-screen transition-colors duration-300">
-        <Header>
-
-        </Header>
-        <main>
-
-        </main>
-        <Footer >
-            
-        </Footer>
-    </div>
+  <div class="container flex min-h-screen transition-colors duration-300">
+    <Header> </Header>
+    <main></main>
+    <Footer> </Footer>
+  </div>
 </template>
