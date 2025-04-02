4dfe7203c309db261f6193361823fd8a3b75e9be
andkcode <akiricuk37@gmail.com>
2025-04-02 17:55:40 +0200

    feat: Integrate PrimeVue UI library
    This commit integrates the PrimeVue UI library into the project.
    
    The changes include:
    - Installing the primevue package.
    - Importing and registering the PrimeVue plugin within the main Vue application.
    - Importing the primeicons css file.
    
    This integration enables the use of PrimeVue components for building the user interface, providing a set of pre-built, customizable, and accessible UI elements.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "../src/App.vue";
+import PrimeVue from 'primevue/config';
 
 
 createApp(App).mount("#app");
+App.use(PrimeVue);
