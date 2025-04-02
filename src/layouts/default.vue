48d0d33f466182f2d90b0da9396330b5533ccf28
andkcode <akiricuk37@gmail.com>
2025-04-02 19:10:09 +0200

    feat: Integrate Clubs page into default layout
    This commit integrates the Clubs page into the default layout, making it accessible as the main content.
    
    The Clubs page was previously not rendered within any layout. This change ensures that the Clubs page is displayed within the application's structure, including the header and footer.
    
    This integration improves the user experience by providing a clear and consistent way to access the Clubs page.

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import Header from '../components/layout/Header.vue';
 import Footer from '../components/layout/Footer.vue';
+import Clubs from '../pages/Clubs.vue';
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen w-full">
     <Header />
 
-    <main class="flex-grow bg-white">
-
+    <main class="flex-grow w-full bg-white">
+      <Clubs />
     </main>
 
     <Footer />
