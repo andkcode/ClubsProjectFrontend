fda190a4b4c64c984f2d765093eb55742d30c208
andkcode <akiricuk37@gmail.com>
2025-04-05 00:19:21 +0200

    feat(components): Implement CardEvent component
    This commit introduces the `CardEvent` component, a reusable UI element for displaying event information.
    
    The component includes:
    - An image representing the event (passed as a prop).
    - The event name (passed as a prop).
    - A "View" button that navigates to the event details page using Vue Router.
    
    This component promotes code reusability and improves the overall structure of the application. It is designed to be easily integrated into various parts of the application where event information needs to be displayed in a consistent manner.

---
+++ src/components/CardEvent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="max-w-[30rem] max-h-[25rem] bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+      <img :src="photo" alt="Photo" class="w-full h-[17rem]" />
+      <div class="p-4">
+        <h1 class="text-xl font-semibold text-gray-900">{{ name }}</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"><router-link :to="`/events/{id}`">View</router-link></button>
+      </div>
+    </div>
+  </template>
+  
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  id: Number,
+  photo: String,
+  name: String,
+})
+</script>
+  
