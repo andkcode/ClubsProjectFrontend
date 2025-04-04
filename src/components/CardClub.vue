11c2897e488c9114edb9a2b35a9bc16c43f5801f
andkcode <akiricuk37@gmail.com>
2025-04-04 22:33:39 +0200

    feat(card): Improve card component styling and structure
    This commit refactors the CardClub.vue component to enhance its visual appearance and improve the overall user experience.
    
    The following changes were implemented:
    
    -   Modified the card's maximum width and height to "max-w-\[30rem] max-h-\[25rem]".
    -   Adjusted the image height to "h-\[17rem]".
    -   Updated the title to use h1 tag.
    -   Modified the description text size to "text-m".
    -   Added custom styles to the "View" button for better visual appeal and user interaction.
    
    These changes result in a more polished and engaging card component.

--- src/components/CardClub.vue
+++ src/components/CardClub.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
-      <img :src="photo" alt="Photo" class="w-full h-auto" />
+    <div class="max-w-[30rem] max-h-[25rem] bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+      <img :src="photo" alt="Photo" class="w-full h-[17rem]" />
       <div class="p-4">
-        <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
-        <p class="text-gray-600 text-sm mb-4">{{ description }}</p>
-        <button><router-link :to="`/clubs/`" class="btn btn-primary">View</router-link></button>
+        <h1 class="text-xl font-semibold text-gray-900">{{ title }}</h1>
+        <p class="text-gray-600 text-m mb-2">{{ description }}</p>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"><router-link :to="`/clubs/`">View</router-link></button>
       </div>
     </div>
   </template>
