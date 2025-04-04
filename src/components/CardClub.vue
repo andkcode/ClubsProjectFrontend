43810d27f00fb4c3301984ffc83d274c9f308faf
andkcode <akiricuk37@gmail.com>
2025-04-05 00:19:06 +0200

    Fix(card): Corrected router link in CardClub component
    The router link in the CardClub component was pointing to a static route `/clubs/`, instead of a dynamic route `/clubs/{id}`. This commit fixes the router link to correctly navigate to the club's specific page.

--- src/components/CardClub.vue
+++ src/components/CardClub.vue
@@ -4,7 +4,7 @@
       <div class="p-4">
         <h1 class="text-xl font-semibold text-gray-900">{{ title }}</h1>
         <p class="text-gray-600 text-m mb-2">{{ description }}</p>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"><router-link :to="`/clubs/`">View</router-link></button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"><router-link :to="`/clubs/{id}`">View</router-link></button>
       </div>
     </div>
   </template>
