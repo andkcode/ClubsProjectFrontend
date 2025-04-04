d34095c26aa56832a7699fcddb82198ceb9a943c
andkcode <akiricuk37@gmail.com>
2025-04-04 19:58:24 +0200

    feat: Remove PrimeVue import and usage
    This commit removes the import statement for `PrimeVue` and its usage within the `main.ts` file.
    
    The `PrimeVue` library was initially included for UI components but is no longer required as the project is migrating to a different UI framework. Removing the unused dependency reduces the bundle size and improves the overall application performance.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "../src/App.vue";
-import PrimeVue from 'primevue/config';
 
 
 createApp(App).mount("#app");
-App.use(PrimeVue);
