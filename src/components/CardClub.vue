5cf0d11e04af17ee1fedc7aceb31e4a1a6d18c7c
andkcode <akiricuk37@gmail.com>
2025-04-04 19:59:11 +0200

    feat(club-card): Implement reusable club card component
    This commit introduces a new `CardClub.vue` component.
    
    This component is designed to display club information in a standardized card format. It accepts props for the club's ID, photo URL, title, and description. The component utilizes these props to render the card with an image, title, description, and a "View" button that links to the club's details page.
    
    This enhances code reusability and maintainability by encapsulating the card's structure and styling into a single, reusable component.

---
+++ src/components/CardClub.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+      <img :src="photo" alt="Photo" class="w-full h-auto" />
+      <div class="p-4">
+        <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
+        <p class="text-gray-600 text-sm mb-4">{{ description }}</p>
+        <button><router-link :to="`/clubs/`" class="btn btn-primary">View</router-link></button>
+      </div>
+    </div>
+  </template>
+  
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  id: Number,
+  photo: String,
+  title: String,
+  description: String
+})
+</script>
+  
