de87445e504589046a06e8c72e3e3ffcc33ce8d6
andkcode <akiricuk37@gmail.com>
2025-04-05 00:19:41 +0200

    feat(events): Implement Events view with data fetching and card display
    This commit introduces the Events view, which fetches event data from the EventsService and displays it using the CardEvent component.
    
    The following changes were made:
    
    - Created the Events.vue component.
    - Implemented data fetching using the EventsService to retrieve all events.
    - Integrated the CardEvent component to display individual events with their photo, name, and ID.
    - Added basic styling for layout and presentation.

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,5 +1,42 @@
 <template>
+    <div class="flex flex-col w-[60%] justify-self-center">
+      <div class="flex flex-col text-center mb-5 mt-5">
+        <h1 class="fw-bolder text-black text-[3.2em] font-bold">Events near you!</h1>
+        <p class="lead fw-normal text-muted mb-0 text-black text-[1.3em]">Events find in your area!</p>
+      </div>
+      <div class="row gx-5">
+        <div class="col-lg-6 mb-2" v-for="event in events" :key="event.id">
+          <CardEvent :id="event.id" :photo="event.photoUrl" :name="event.name" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+<script>
+import CardEvent from '../components/CardEvent.vue';
+import EventsService from '../composables/EventsService';
 
-</template>
-<script type="ts">
-</script>
+export default {
+  name: 'Events',
+  components: { CardEvent },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  methods: {
+    async getAllEvents() {
+      try {
+        const response = await EventsService.getAllEvents();
+        this.events = response;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  created() {
+    this.getAllEvents();
+  },
+};
+
+  </script>
