8a98167c548a4707d7cb114bc9243a85b9a0c25e
andkcode <akiricuk37@gmail.com>
2025-05-01 23:49:32 +0200

    feat: Refresh authentication on app mount
    This commit introduces a mechanism to automatically refresh the user's authentication status when the application is mounted.
    
    The `useAuth` composable now includes a `refreshAuth` method, which is called upon application initialization within `App.vue`. This ensures that the application accurately reflects the user's current authentication state, especially after a page refresh or when the authentication token might have expired. This improves the user experience by avoiding unnecessary redirects or authentication prompts.

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,9 @@
 
 <script setup lang="ts">
 import MainView from '@/layouts/default.vue';
+import { useAuth } from './composables/useAuth';
+
+const { refreshAuth } = useAuth();
+refreshAuth();
 </script>
 
