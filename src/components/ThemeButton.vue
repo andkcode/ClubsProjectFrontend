377b2669932b09d65c9c0dc3f5cc07832eb9c694
andkcode <akiricuk37@gmail.com>
2025-05-13 22:44:47 +0200

    fix(theme): Correct text color for theme button and user menu icons
    The text color for the theme button and user menu icons was hardcoded to `text-white` and `text-gray-300` respectively, which did not adapt to the current theme. This commit changes the color to `text-[hsl(var(--foreground))]` to ensure the text color is always appropriate for the selected theme (light or dark).

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -20,7 +20,7 @@ console.log(mode.value)
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger as-child>
-      <Button variant="outline" class="relative text-white">
+      <Button variant="outline" class="relative text-[hsl(var(--foreground))]">
         <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span class="sr-only">Toggle theme</span>
