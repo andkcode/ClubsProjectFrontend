dd39890a92282528474157a4e27478a58b6d372e
andkcode <akiricuk37@gmail.com>
2025-05-20 01:24:59 +0200

    feat: Integrate vue-sonner for global notifications
    This commit introduces `vue-sonner` as the global notification system within the application.
    
    The primary motivation for this change is to provide a consistent and visually appealing way to display notifications to the user, improving the overall user experience. `vue-sonner` was chosen for its ease of use, customizability, and accessibility features.
    
    The following changes were made:
    - Added `vue-sonner` as a project dependency.
    - Imported and rendered the `<Toaster>` component in `App.vue` to enable global notifications.
    - Configured the `<Toaster>` component with `richColors` and `top-right` positioning for optimal visual presentation.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <MainView />
+  <Toaster richColors position="top-right"/>
 </template>
 
 <script setup lang="ts">
 import MainView from '@/layouts/default.vue';
 import { useAuth } from './composables/useAuth';
+import { Toaster } from 'vue-sonner';
 
 const { refreshAuth } = useAuth();
 refreshAuth();
